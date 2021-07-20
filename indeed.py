65c186eb743497f10df998a28db77c519766aa7b
deokhwa_lee <deokhwa_lee@tmax.co.kr>
2021-07-20 10:58:34 +0900

    Finish extracting indeed page adding location and link

--- indeed.py
+++ indeed.py
@@ -1,63 +1,53 @@
 # Install library -> resquests, beautifulsoup4
 import requests
 from bs4 import BeautifulSoup
-import sys
 
 RADIUS = 100
 LIMIT = 50
 URL = f"https://kr.indeed.com/%EC%B7%A8%EC%97%85?as_and=Python&as_phr=&as_any=&as_not=&as_ttl=&as_cmp=&jt=all&st=&salary=&radius={RADIUS}&l=%EA%B2%BD%EA%B8%B0%EB%8F%84+%EC%95%88%EC%96%91&fromage=any&limit={LIMIT}&sort=&psf=advsrch&from=advancedsearch"
 
+
 def extract_indeed_pages():
     url = requests.get(URL)
     # print(url) # 200 means okay
     soup = BeautifulSoup(url.text, "html.parser")
-    # print(soup)
     pagination = soup.find("div", {"class": "pagination"})
-    # print(pagination)
 
     links = pagination.find_all("a")
-    # print(links)
     pages = []
-    for link in links[:-1]: # To elimiante last element
+    for link in links[:-1]:  # To elimiante last element
         pages.append(int(link.find("span").string))
-    # print(pages)
     max_page = pages[-1]
     return max_page
 
+
+def extract_job(html):
+    spans = html.find_all("span")
+    for span in spans:
+        if span.get("title") is not None:
+            title = span.get("title")
+    company = html.find("span", {"class": "companyName"}).string
+    location = html.find("div", {"class": "companyLocation"}).string
+    job_id = html.find_parent("a")["data-jk"]
+    return {
+        'title': title,
+        'company': company,
+        'location': location,
+        'link': f"https://kr.indeed.com/%EC%B7%A8%EC%97%85?as_and=Python&as_phr&as_any&as_not&as_ttl&as_cmp&jt=all&st&salary&radius=100&l=%EA%B2%BD%EA%B8%B0%EB%8F%84%20%EC%95%88%EC%96%91&fromage=any&limit=50&sort&psf=advsrch&from=advancedsearch&vjk={job_id}"
+    }
+
+
 def extract_indeed_jobs(last_page):
     jobs = []
-    
-    # Read all the pages
     for page in range(last_page):
+        # print(f"Scrapping page {page}")
         each_page = requests.get(f"{URL}&start={page * LIMIT}")
-        # print(each_page.status_code)
         soup = BeautifulSoup(each_page.text, "html.parser")
-        results = soup.find_all("div", {"class": "job_seen_beacon"})
-
-
-        each_page = requests.get(f"{URL}&start={0 * LIMIT}")
-        soup = BeautifulSoup(each_page.text, "html.parser")
-        results = soup.find_all("div", {"class": "job_seen_beacon"}) # job cards
-        # print(len(results)) # 50 job cards in a page
-        for result in results:
-            spans = result.find_all("span")
-            for span in spans:
-                # company = span.find("span", {"class": "companyName"}) # None
-                # span.find("span")['class'] # None
-                if span.get("class") == ['companyName']:
-                    company = span.string
-                if span.get("title") is not None:
-                    title = span.get("title")
-            print(title, company)
-                
-
-    # Only bring title
-    # each_page = requests.get(f"{URL}&start={0 * LIMIT}")
-    # soup = BeautifulSoup(each_page.text, "html.parser")
-    # results = soup.find_all("h2", {"class": "jobTitle jobTitle-color-purple"})
-    # for result in results:
-    #     title = result.find("span")["title"]
-    #     print(title)
-    # # print(results)
-
+        job_cards_zone = soup.find("div", {"id": "mosaic-zone-jobcards"})
+        job_cards = job_cards_zone.find_all(
+            "div", {"class": "job_seen_beacon"})
+        # print(len(job_cards)) # 50 job cards in a page
+        for job_card in job_cards:
+            job = extract_job(job_card)
+            jobs.append(job)
     return jobs
