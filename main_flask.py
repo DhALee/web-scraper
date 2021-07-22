e002ce40832528f29cad87b0d88211d7fd85f498
deokhwa_lee <deokhwa_lee@tmax.co.kr>
2021-07-22 12:36:52 +0900

    Show search result on page

--- main_flask.py
+++ main_flask.py
@@ -1,24 +1,37 @@
 from flask import Flask, render_template, request, redirect
-from scraper
+from scraper_flask import get_jobs
 
-app = Flask("web-scrapper")
+app = Flask("job-scraper")
+# db should be out of router
+db = {}  # fake database that store jobs to save searching time when user find same career that others searched before
 
 # "@" is decorator which takes a function right under itself
-@app.route("/") # "/" means root
-def home():
+@app.route("/")  # "/" means root
+def index():
     return render_template("index.html")
 
-@app.route("/<username>") # "<>" is placeholder
+
+@app.route("/<username>")  # "<>" is placeholder
 def contact(username):
     return f"Hello {username}, how are you today?"
 
-@app.route("/career")
-def report():
+
+@app.route("/result")
+def result():
     career = request.args.get("career")
     if career:
-        career = career.lower() # in case when user type in capital words
+        career = career.lower()  # in case when user type in capital words
+        existing_jobs = db.get(career)
+        if existing_jobs:  # if db has career, return that data
+            jobs = existing_jobs
+        else:
+            jobs = get_jobs(career)
+            db[career] = jobs
     else:
-        return redirect("/") # when input comes nothing go back to root page
-    return render_template("career.html", search=career)
+        return redirect("/")  # when input comes nothing go back to root page
+        
+    # keyword "search" which could be any other words send variable "career" into result.html
+    return render_template("result.html", result_num=len(jobs), search=career, jobs=jobs) 
 
-app.run(host="0.0.0.0")
+
+app.run(host="0.0.0.0", debug=True)
