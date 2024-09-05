47d608c396dc63fadba35ddc7d049cc9b1d98f3c
deok0726 <deok0726@gmail.com>
2024-09-05 22:46:27 +0900

    Add modal popup text read

--- web.py
+++ web.py
@@ -8,9 +8,10 @@ from bs4 import BeautifulSoup
 from langchain_community.document_loaders import WebBaseLoader
 from langchain_community.document_loaders.sitemap import SitemapLoader
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
@@ -129,7 +130,6 @@ def hc_event(url):
             # else:
             #     print("No match found.")
 
-
     driver.quit()
 
 def read_text(url):
@@ -158,8 +158,79 @@ def read_text(url):
     # 브라우저 닫기
     driver.quit()
 
+def cg_event(url):
+    chrome_options = Options()
+    chrome_options.add_argument('--no-sandbox')  # 샌드박스 비활성화
+    chrome_options.add_argument('--disable-dev-shm-usage')  # /dev/shm 사용 비활성화
+    chrome_options.add_argument('--remote-debugging-port=9222')  # 원격 디버깅 포트 설정
+    chrome_options.add_argument('--headless')  # GUI 없이 실행 (헤드리스 모드)
+    chrome_options.add_argument('--disable-gpu')  # GPU 비활성화 (일부 환경에서 필요)
+    chrome_options.add_argument('--disable-software-rasterizer')  # 소프트웨어 래스터라이저 비활성화
+    service = Service('/usr/bin/chromedriver')
+    driver = webdriver.Chrome(service=service, options=chrome_options)
+
+    url = url  # 원하는 URL로 변경하세요
+    driver.get(url)
+
+    # 자바스크립트가 실행되도록 잠시 대기
+    time.sleep(5)  # 필요 시 적절한 시간으로 조정 가능
+
+    page_source = driver.page_source
+    soup = BeautifulSoup(page_source, 'html.parser')
+    
+    modals = driver.find_elements(By.CSS_SELECTOR, '.ctnr a')
+
+    for idx, modal in enumerate(modals):
+        try:
+            modal.click()
+            WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.ID, 'modal')))
+            modal_content = driver.find_element(By.ID, 'modal').text
+            # print(modal_content)
+
+            if modal_content.strip():
+                with open(f'/svc/project/genaipilot/web-scraper/cg_files/modal_{idx}.txt', 'w', encoding='utf-8') as file:
+                    print(f"modal_{idx}.txt 저장 중")
+                    file.write(modal_content)
+
+            webdriver.ActionChains(driver).send_keys(Keys.ESCAPE).perform()
+            WebDriverWait(driver, 5).until(EC.invisibility_of_element_located((By.ID, 'modal')))
+        
+        except Exception as e:
+            # print(f"Failed to process popup {idx}: {e}")
+            continue
+
+    links = []
+    for link in soup.find_all('a', href=True):
+        links.append(link['href'])
+
+    filtered_links = [element for element in links if '/card/detail/' in element]
+    card_links = list(set(filtered_links))
+    # print(card_links)
+    # print(len(card_links))
+    cg = 'https://www.card-gorilla.com'
+    event_links = []
+    for card_link in card_links:
+        event_links.append(cg + card_link)
+
+    for event_link in event_links:
+        driver.get(event_link)
+        print(event_link)
+        name = event_link.split('/')[-1]
+        time.sleep(3)
+        page_source = driver.page_source
+        soup = BeautifulSoup(page_source, 'html.parser')
+        page_text = soup.get_text()
+
+        with open(f'/svc/project/genaipilot/web-scraper/cg_files/{name}.txt', 'w', encoding='utf-8') as file:
+            print(f"{name}.txt 저장 중")
+            file.write(page_text)
+
+    driver.quit()
+
 
 if __name__ == "__main__":
     # url = 'https://www.hyundaicard.com/cpb/ev/CPBEV0101_01.hc'
+    url = 'https://www.card-gorilla.com/event'
     # hc_event(url)
-   read_text('https://www.hyundaicard.com/cpb/ev/CPBEV0101_06.hc?bnftWebEvntCd=208551&searchWord=')
+    cg_event(url)
+
